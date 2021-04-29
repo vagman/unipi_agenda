bcc184ac8a53b56013b186d88d600877d59b8607
Ilias Brinias <elliotpatra@gmai.com>
2021-04-29 17:53:34 +0300

    register is completed from backend

--- src/main/java/com/exc/unipi_agenda/controllers/RegisterController.java
+++ src/main/java/com/exc/unipi_agenda/controllers/RegisterController.java
@@ -1,11 +1,14 @@
 package com.exc.unipi_agenda.controllers;
 
 import com.exc.unipi_agenda.model.User;
+import org.springframework.boot.web.servlet.server.Session;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpSession;
 
 @Controller
 public class RegisterController {
@@ -15,22 +18,17 @@ public class RegisterController {
     }
 
     @PostMapping("/register")
-    public String registerUser(Model model,
+    public String registerUser(Model model,HttpSession session ,
                                @RequestParam(name = "username", required = false) String username,
                                @RequestParam(name = "password", required = false) String password,
                                @RequestParam(name = "firstname", required = false) String firstname,
                                @RequestParam(name = "lastname", required = false) String lastname) {
-//        User u = User.register(username,password,firstname,lastname,model);
-        System.out.println(firstname+lastname+username+password);
+        User u = User.register(username,password,firstname,lastname,model);
+        model.addAttribute("user",u);
+        if (u != null){
+//            TODO: 26/4/2021 after registration go to main page
+            System.out.println("the user registered successfully");
+        }
         return "signup";
-//        if (u != null){
-//            // TODO: 26/4/2021 after registration go to main page
-////            model.addAttribute("message", "all ok");
-//            return "signup";
-//        }else {
-//            return "signup";
-//        }
-
-
     }
 }
