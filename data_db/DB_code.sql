792bbbbd3ced496070ec5af648460bc62f73f465
Ilias Brinias <elliotpatra@mail.com>
2021-07-08 19:22:33 +0300

    1. Notifications and invitation are in one list 2. New trigger for messages to participants about invitations 3. Database table meeting_participants contains date 4. MeetingComment.send() function created

--- data_db/DB_code.sql
+++ data_db/DB_code.sql
@@ -35,16 +35,6 @@ CREATE TABLE `meeting` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `meeting`
---
-
-LOCK TABLES `meeting` WRITE;
-/*!40000 ALTER TABLE `meeting` DISABLE KEYS */;
-INSERT INTO `meeting` VALUES (1,'meeting test','2021-05-24 19:53:00',12.6,'Brinel'),(2,'meeting test','2021-09-10 09:00:00',12.6,'Brinel'),(3,'meeting test1','2021-09-10 09:00:00',12.6,'prekwood'),(4,'meeting test2','2021-09-11 10:00:00',12.6,'Brinel'),(5,'meeting test3','2021-09-12 09:30:00',12.6,'vagman'),(6,'meeting test4','2021-09-13 11:00:00',12.6,'Brinel'),(7,'meeting test5','2021-09-14 12:45:00',12.6,'prekwood'),(8,'meeting test6','2021-09-15 09:00:00',12.6,'Brinel'),(9,'meeting test7','2021-09-16 09:00:00',12.6,'vagman'),(10,'meeting test  111','2100-09-26 10:00:00',12.5,'Brinel'),(11,'meeting test 110','2100-09-26 10:00:00',12.5,'Brinel'),(12,'meeting test 120','2100-09-26 10:00:00',12.5,'Brinel'),(13,'meeting test 121','2100-09-26 10:00:00',12.5,'Brinel'),(14,'meeting test 122','2100-09-26 10:00:00',12.5,'Brinel'),(15,'meeting test 132','2100-09-26 10:00:00',12.5,'Brinel'),(16,'meeting test 132','2100-09-26 10:00:00',12.5,'Brinel');
-/*!40000 ALTER TABLE `meeting` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
 -- Table structure for table `meeting_comments`
 --
 
@@ -53,21 +43,14 @@ DROP TABLE IF EXISTS `meeting_comments`;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `meeting_comments` (
   `id_meeting` int NOT NULL,
-  `id_user` int NOT NULL,
+  `username` varchar(30) NOT NULL,
   `date` datetime NOT NULL,
   `msg` text,
-  PRIMARY KEY (`id_meeting`,`id_user`,`date`)
+  PRIMARY KEY (`id_meeting`,`username`,`date`),
+  KEY `username_idx` (`username`),
+  CONSTRAINT `username` FOREIGN KEY (`username`) REFERENCES `users` (`username`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `meeting_comments`
---
-
-LOCK TABLES `meeting_comments` WRITE;
-/*!40000 ALTER TABLE `meeting_comments` DISABLE KEYS */;
-/*!40000 ALTER TABLE `meeting_comments` ENABLE KEYS */;
-UNLOCK TABLES;
 
 --
 -- Table structure for table `meeting_participants`
@@ -80,20 +63,11 @@ CREATE TABLE `meeting_participants` (
   `id_meeting` int NOT NULL,
   `username` varchar(30) NOT NULL,
   `invitation_status` varchar(10) DEFAULT NULL,
+  `date` datetime DEFAULT NULL,
   PRIMARY KEY (`id_meeting`,`username`),
   CONSTRAINT `meeting_participants_chk_1` CHECK ((`invitation_status` in (_utf8mb4'open',_utf8mb4'approved',_utf8mb4'declined')))
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `meeting_participants`
---
-
-LOCK TABLES `meeting_participants` WRITE;
-/*!40000 ALTER TABLE `meeting_participants` DISABLE KEYS */;
-INSERT INTO `meeting_participants` VALUES (3,'Brinel','approved'),(3,'vagman','approved'),(9,'Brinel','approved'),(10,'prekwood','open'),(10,'vagman','open'),(11,'prekwood','open'),(11,'vagman','open'),(12,'prekwood','open'),(12,'vagman','open'),(13,'prekwood','open'),(13,'vagman','open'),(14,'prekwood','open'),(14,'vagman','open'),(15,'prekwood','open'),(15,'vagman','open');
-/*!40000 ALTER TABLE `meeting_participants` ENABLE KEYS */;
-UNLOCK TABLES;
 
 --
 -- Table structure for table `user_notification`
@@ -109,18 +83,8 @@ CREATE TABLE `user_notification` (
   `date` datetime DEFAULT NULL,
   `viewed` tinyint(1) DEFAULT NULL,
   PRIMARY KEY (`id_notification`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `user_notification`
---
-
-LOCK TABLES `user_notification` WRITE;
-/*!40000 ALTER TABLE `user_notification` DISABLE KEYS */;
-INSERT INTO `user_notification` VALUES (1,'Brinel','hello','2022-10-07 08:08:08',0),(2,'Brinel','hello2','2022-11-07 08:08:08',0),(3,'Brinel','hello2','2021-06-04 08:08:08',0);
-/*!40000 ALTER TABLE `user_notification` ENABLE KEYS */;
-UNLOCK TABLES;
 
 --
 -- Table structure for table `users`
@@ -139,16 +103,6 @@ CREATE TABLE `users` (
   PRIMARY KEY (`username`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `users`
---
-
-LOCK TABLES `users` WRITE;
-/*!40000 ALTER TABLE `users` DISABLE KEYS */;
-INSERT INTO `users` VALUES ('Brinel','113bec15a0ed6309b845d420e0fc0db7','[B@5f7eaac9','Ilias','Brinias',NULL),('prekwood','113bec15a0ed6309b845d420e0fc0db7','[B@5f7eaac9','Nikos','Prekas',NULL),('vagman','113bec15a0ed6309b845d420e0fc0db7','[B@5f7eaac9','Vaggelis','Manousakis',NULL);
-/*!40000 ALTER TABLE `users` ENABLE KEYS */;
-UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -159,4 +113,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2021-06-04 19:13:41
+-- Dump completed on 2021-07-08 19:22:22
