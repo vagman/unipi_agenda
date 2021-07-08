3e4887d67db4ed114e124c689daf75fc1499a3c1
Ilias Brinias <elliotpatra@mail.com>
2021-07-08 19:21:39 +0300

    1. Notifications and invitation are in one list 2. New trigger for messages to participants about invitations 3. Database table meeting_participants contains date 4. MeetingComment.send() function created

--- src/main/java/com/exc/unipi_agenda/model/MeetingComment.java
+++ src/main/java/com/exc/unipi_agenda/model/MeetingComment.java
@@ -1,9 +1,57 @@
 package com.exc.unipi_agenda.model;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.Date;
 
 public class MeetingComment {
     private User user;
     private String message;
-    private Date date;
+    private String date;
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    static boolean send(int id_meeting, String username, String message){
+        Connection conn = Db.getConnection();
+        if (conn == null) {
+            return false;
+        }
+        // load name
+        String sql_query = "INSERT INTO meeting_comments(id_meeting, username, date, msg)\n"+
+                            "VALUES(?,?,NOW(),?)";
+        try{
+            PreparedStatement ps = conn.prepareStatement(sql_query);
+            ps.setInt(1,id_meeting);
+            ps.setString(2, username);
+            ps.setString(3, message);
+            return ps.execute();
+
+        }catch (SQLException throwables) {
+            throwables.printStackTrace();
+            return false;
+        }
+    }
 }
